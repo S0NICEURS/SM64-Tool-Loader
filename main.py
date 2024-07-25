4daf642f1775ae1bc2bb6db954c63a402f949289
Soniceurs <120643438+S0NICEURS@users.noreply.github.com>
2024-07-26 01:35:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import subprocess
 import importlib
+import json
+import os
 
-# Modules à vérifier et installer si nécessaire
+# Chemin vers le fichier JSON
+json_file = 'modules_installed.json'
+
+# Fonction pour vérifier et installer les modules
+def check_and_install_modules(modules):
+    for module in modules:
+        try:
+            importlib.import_module(module)
+            print(f"{module} is already installed.")
+        except ImportError:
+            print(f"{module} is not installed, install in progress...")
+            subprocess.check_call(['pip', 'install', module])
+
+# Liste des modules nécessaires
 required_modules = [
     'tkinter',
     'pillow',
@@ -17,14 +32,14 @@ required_modules = [
     'tempfile',
 ]
 
-# Vérifier et installer les modules nécessaires
-for module in required_modules:
-    try:
-        importlib.import_module(module)
-        print(f"{module} is already installed.")
-    except ImportError:
-        print(f"{module} is not installed, install in progress...")
-        subprocess.check_call(['pip', 'install', module])
-
-# Une fois les modules installés, ouvrir SM64 Tool Loader.py
+# Vérifier si le fichier JSON existe
+if not os.path.exists(json_file):
+    # Vérifier et installer les modules nécessaires
+    check_and_install_modules(required_modules)
+    
+    # Créer le fichier JSON pour indiquer que les modules sont installés
+    with open(json_file, 'w') as file:
+        json.dump({"modules_installed": True}, file)
+        
+# Lancer le script Python désiré
 subprocess.run(['python', 'SM64 Tool Loader.py'])
